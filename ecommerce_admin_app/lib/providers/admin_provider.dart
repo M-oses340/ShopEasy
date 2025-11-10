eda4880b079461ca240a38ec6f988db9162b3656
Moses Omwa <mosesomwa7@gmail.com>
2025-11-10 10:12:47 +0300

    DbService

--- ecommerce_admin_app/lib/providers/admin_provider.dart
+++ ecommerce_admin_app/lib/providers/admin_provider.dart
@@ -1,14 +1,15 @@
 import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce_admin_app/controllers/db_service.dart';
 import 'package:flutter/material.dart';
 
-class AdminProvider extends ChangeNotifier{
+class AdminProvider extends ChangeNotifier {
   List<QueryDocumentSnapshot> categories = [];
   StreamSubscription<QuerySnapshot>? _categorySubscription;
+
   List<QueryDocumentSnapshot> products = [];
   StreamSubscription<QuerySnapshot>? _productsSubscription;
+
   List<QueryDocumentSnapshot> orders = [];
   StreamSubscription<QuerySnapshot>? _ordersSubscription;
 
@@ -18,75 +19,78 @@ class AdminProvider extends ChangeNotifier{
   int ordersDelivered = 0;
   int ordersCancelled = 0;
   int ordersOnTheWay = 0;
-  int orderPendingProcess=0;
+  int orderPendingProcess = 0;
 
-  AdminProvider(){
+  AdminProvider() {
     getCategories();
     getProducts();
     readOrders();
   }
 
   // GET all the categories
-    void getCategories() {
+  void getCategories() {
     _categorySubscription?.cancel();
-    _categorySubscription = DbService().readCategories().listen((snapshot) {
+    _categorySubscription = DbService.instance.readCategories().listen((snapshot) {
       categories = snapshot.docs;
-      totalCategories=snapshot.docs.length;
-      notifyListeners();
-    });
-  }
-  // GET all the products
-    void getProducts() {
-    _productsSubscription?.cancel();
-    _productsSubscription = DbService().readProducts().listen((snapshot) {
-      products = snapshot.docs;
-      totalProducts=snapshot.docs.length;
+      totalCategories = snapshot.docs.length;
       notifyListeners();
     });
   }
 
-  // read all the orders
-  void readOrders(){
-     _ordersSubscription?.cancel();
-    _ordersSubscription = DbService().readOrders().listen((snapshot) {
+  // GET all the products
+  void getProducts() {
+    _productsSubscription?.cancel();
+    _productsSubscription = DbService.instance.readProducts().listen((snapshot) {
+      products = snapshot.docs;
+      totalProducts = snapshot.docs.length;
+      notifyListeners();
+    });
+  }
+
+  // Read all the orders
+  void readOrders() {
+    _ordersSubscription?.cancel();
+    _ordersSubscription = DbService.instance.readOrders().listen((snapshot) {
       orders = snapshot.docs;
-      totalOrders=snapshot.docs.length;
+      totalOrders = snapshot.docs.length;
       setOrderStatusCount();
       notifyListeners();
     });
   }
 
-  // to the various order types
-   void setOrderStatusCount(){
+  // Count orders by status
+  void setOrderStatusCount() {
     ordersDelivered = 0;
     ordersCancelled = 0;
     ordersOnTheWay = 0;
-    orderPendingProcess=0;
-    for(int i = 0; i < orders.length; i++){
-      if(orders[i]["status"] == "DELIVERED"){
-        ordersDelivered++;
-      }else if(orders[i]["status"] == "CANCELLED"){
-        ordersCancelled++;
-      }else if(orders[i]["status"] == "ON_THE_WAY"){
-        ordersOnTheWay++;
-      }
-      else{
-        orderPendingProcess++;
+    orderPendingProcess = 0;
+
+    for (final order in orders) {
+      switch (order["status"]) {
+        case "DELIVERED":
+          ordersDelivered++;
+          break;
+        case "CANCELLED":
+          ordersCancelled++;
+          break;
+        case "ON_THE_WAY":
+          ordersOnTheWay++;
+          break;
+        default:
+          orderPendingProcess++;
       }
     }
-    notifyListeners();
   }
 
-void cancelProvider(){
-  _ordersSubscription?.cancel();
-  _productsSubscription?.cancel();
-  _categorySubscription?.cancel();
-}
+  void cancelProvider() {
+    _ordersSubscription?.cancel();
+    _productsSubscription?.cancel();
+    _categorySubscription?.cancel();
+  }
 
-@override
+  @override
   void dispose() {
     cancelProvider();
     super.dispose();
   }
-
 }
